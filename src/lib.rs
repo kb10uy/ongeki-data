d924a3adadfdcd38fbb5b98b3975502902cbeb1b
kb10uy <kb10uy@kb10uy.org>
2019-04-04 00:12:10 +0900

    キャラ関連構造体

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub struct VoiceActor {
+    pub name: String,
+    pub reading: String,
+}
+
+pub struct Character {
+    pub name: String,
+    pub reading: String,
+    pub voice_actor: String,
+}
+
+pub struct Unit {
+    pub name: String,
+    pub reading: String,
+}
