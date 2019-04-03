aa08020d8f717684425f7da836552247196a020a
kb10uy <kb10uy@kb10uy.org>
2019-04-04 01:13:40 +0900

    構造体を追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,46 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
 pub struct VoiceActor {
-    pub name: String,
-    pub reading: String,
+    pub name: Vec<String>,
+    pub reading: Vec<String>,
 }
 
+#[derive(Deserialize)]
 pub struct Character {
-    pub name: String,
-    pub reading: String,
-    pub voice_actor: String,
+    pub name: Vec<String>,
+    pub reading: Vec<String>,
+    pub voice_actor: VoiceActor,
 }
 
+#[derive(Deserialize)]
 pub struct Unit {
     pub name: String,
     pub reading: String,
+}
+
+#[derive(Deserialize)]
+pub struct Composer {
+    pub name: String,
+    pub reading: String,
+}
+
+#[derive(Deserialize)]
+pub struct Song {
+    pub title: String,
+    pub reading: String,
+    pub composer: Composer,
+}
+
+#[derive(Deserialize)]
+pub struct CharactersDefinition {
+    pub updated_at: String,
+    pub characters: Vec<Character>,
+    pub units: Vec<Unit>,
+}
+
+#[derive(Deserialize)]
+pub struct SongsDefinition {
+    pub updated_at: String,
+    pub songs: Vec<Song>,
 }
